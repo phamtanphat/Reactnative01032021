f1129ceb44e0d56a8a3f2c71c70b64c9fa48442a
phamtanphat <phatdroid94@gmail.com>
2021-03-17 20:38:10 +0700

    toan tu ba ngoi

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,15 +1,29 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import { Text, SafeAreaView } from 'react-native';
 
 export default class MainScreen extends Component {
+
+    // shouldShowVn(isMemorized, vn) {
+    //     if (isMemorized) {
+    //         return '----';
+    //     } else {
+    //         return vn;
+    //     }
+    // }
+
     render() {
         // destructuring
-        const { en, vn } = { en: 'One', vn: "Mot" }
+        const { vn, en, isMemorized } = { en: 'One', vn: 'Mot', isMemorized: true };
         return (
-            <SafeAreaView style={{ flex: 1 }}>
-                <Text> {en} </Text>
-                <Text> {vn} </Text>
+            <SafeAreaView style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+            }}>
+                <Text style={{ fontSize: 30 }}> {en} </Text>
+                <Text style={{ fontSize: 30 }}> {isMemorized ? "----" : vn} </Text>
             </SafeAreaView>
         );
     }
