44be646acb3799f4676b38e2bdc979e91e2d833a
phamtanphat <phatdroid94@gmail.com>
2021-03-17 20:10:12 +0700

    tao screen main

--- App.js
+++ App.js
@@ -1,91 +1,9 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable react-native/no-inline-styles */
 import React, { Component } from 'react';
-import { Text, View, SafeAreaView } from 'react-native';
+import MainScreen from './src/screens/MainScreen';
 
 export default class App extends Component {
   render() {
-    return (
-      <SafeAreaView
-        style={{ flex: 1, flexDirection: 'column', alignItems: 'stretch' }}>
-        <View style={{ flex: 1, backgroundColor: 'red' }}>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'yellow',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>A</Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'orange',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>B</Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'gray',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>C</Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'pink',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>D</Text>
-          </View>
-        </View>
-
-        <View style={{ flex: 1, backgroundColor: 'blue', flexDirection: 'row' }}>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'yellow',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>A</Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'orange',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>B</Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'gray',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>C</Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: 'pink',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>D</Text>
-          </View>
-        </View>
-      </SafeAreaView>
-    );
+    return <MainScreen />;
   }
 }
---
+++ src/screens/MainScreen.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { Text, View, SafeAreaView } from 'react-native';
+
+export default class MainScreen extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text> textInComponent </Text>
+            </SafeAreaView>
+        );
+    }
+}
