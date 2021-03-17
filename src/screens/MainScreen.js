bb39a9e3afe07533faaf37f5658feb757e7a9038
phamtanphat <phatdroid94@gmail.com>
2021-03-17 20:23:29 +0700

    destructuring

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,12 +1,15 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { Text, View, SafeAreaView } from 'react-native';
+import { Text, SafeAreaView } from 'react-native';
 
 export default class MainScreen extends Component {
     render() {
+        // destructuring
+        const { en, vn } = { en: 'One', vn: "Mot" }
         return (
-            <SafeAreaView>
-                <Text> textInComponent </Text>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Text> {en} </Text>
+                <Text> {vn} </Text>
             </SafeAreaView>
         );
     }
