5a18ed1e5dfebecb3f2f64bbd39744fc3a8b12f5
phamtanphat <phatdroid94@gmail.com>
2021-03-19 19:48:50 +0700

    ui list word

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,38 +1,107 @@
+/* eslint-disable no-shadow */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { Text, SafeAreaView, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Text, SafeAreaView, View, TouchableOpacity, StyleSheet } from 'react-native';
 import screenDimension from '../helpers/screenDimension';
+
 
 export default class MainScreen extends Component {
 
     render() {
+
+        const word = [
+            { id: 1, en: "One", vn: "Một", isMemorized: true },
+            { id: 2, en: "Two", vn: "Hai", isMemorized: true },
+            { id: 3, en: "Three", vn: "Ba", isMemorized: false },
+            { id: 4, en: "Four", vn: "Bốn", isMemorized: false },
+            { id: 5, en: "Five", vn: "Năm", isMemorized: true },
+        ];
+
         return (
             <SafeAreaView style={{
                 flex: 1,
                 flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
             }}>
-                <TouchableOpacity
-                    onPress={function () {
-                        alert("hello")
-                    }}
-                    activeOpacity={0.5}
-                    style={{
-                        backgroundColor: 'orange',
-                        width: screenDimension.getWidth() / 4.5,
-                        height: 100,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        borderWidth: 1,
-                        borderRadius: 5,
-                    }}>
-                    <View>
-                        <Text>Click</Text>
-                    </View>
-                </TouchableOpacity>
+                {
+                    word.map(word => {
+                        return (
+                            <View key={word.id}>
+                                <View style={styles.groupWord}>
+                                    <View style={styles.groupHorizontal}>
+                                        <Text style={styles.textEn}>{word.en}</Text>
+                                        <Text style={styles.textVn}>
+                                            {word.isMemorized ? '----' : word.vn}
+                                        </Text>
+                                    </View>
+                                    <View style={styles.groupHorizontal}>
+                                        <TouchableOpacity
+                                            style={{
+                                                ...styles.buttonMemorize,
+                                                backgroundColor: word.isMemorized ? 'green' : 'red',
+                                            }}>
+                                            <Text style={styles.textMemorize}>
+                                                {word.isMemorized ? 'Forgot' : 'Memorize'}
+                                            </Text>
+                                        </TouchableOpacity>
+                                        <TouchableOpacity
+                                            style={styles.buttonRemove}>
+                                            <Text style={styles.textRemove}>Remove</Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </View>
+                            </View>
+                        );
+                    })
+                }
             </SafeAreaView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    groupWord: {
+        height: 100,
+        backgroundColor: 'gainsboro',
+        justifyContent: 'space-evenly',
+        marginHorizontal: 10,
+        borderRadius: 5,
+        marginBottom: 10,
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+        shadowColor: '#000',
+    },
+    groupHorizontal: {
+        marginBottom: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    textEn: {
+        color: 'green',
+        fontWeight: '500',
+        fontSize: screenDimension.getWidth() / 22,
+    },
+    textVn: {
+        color: 'red',
+        fontWeight: '500',
+        fontSize: screenDimension.getWidth() / 22,
+    },
+    buttonMemorize: {
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonRemove: {
+        backgroundColor: 'yellow',
+        padding: 10,
+        borderRadius: 5,
+    },
+    textMemorize: {
+        color: 'white',
+        fontSize: screenDimension.getWidth() / 22,
+    },
+    textRemove: {
+        color: 'darkblue',
+        fontSize: screenDimension.getWidth() / 22,
+    },
+});
