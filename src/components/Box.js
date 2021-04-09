55c3e505c618df6afec647dbba7b391928939a38
phamtanphat <phatdroid94@gmail.com>
2021-04-09 21:03:13 +0700

    sử dụng props

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,30 +1,24 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React from 'react';
+import {View, Text} from 'react-native';
 import Child from './Child';
-import {connect} from 'react-redux';
 
-class Box extends Component {
-  render() {
-    return (
-      <View style={{justifyContent: 'center', flex: 1}}>
-        <Text
-          style={{
-            alignSelf: 'center',
-            color: 'red',
-            fontSize: 20,
-            marginBottom: 10,
-          }}>
-          Count = {this.props.count}
-        </Text>
-        <Child />
-      </View>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {count: state};
+const Box = (props) => {
+  const {count} = props;
+  return (
+    <View style={{justifyContent: 'center', flex: 1}}>
+      <Text
+        style={{
+          alignSelf: 'center',
+          color: 'red',
+          fontSize: 20,
+          marginBottom: 10,
+        }}>
+        Count = {count}
+      </Text>
+      <Child />
+    </View>
+  );
 };
 
-export default connect(mapStateToProps)(Box);
+export default Box;
--- src/components/Child.js
+++ src/components/Child.js
@@ -1,35 +1,28 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {PureComponent} from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
-import {connect} from 'react-redux';
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
 
-class Child extends PureComponent {
-  render() {
-    console.log('Render Child');
-    return (
-      <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
-        <TouchableOpacity
-          onPress={() => this.props.dispatch({type: 'INCREASE'})}
-          style={{padding: 10, backgroundColor: 'green', borderRadius: 5}}>
-          <Text>InCrease</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => this.props.dispatch({ type: 'DECREASE' })}
-          style={{padding: 10, backgroundColor: 'red', borderRadius: 5}}>
-          <Text>DeCrease</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => this.props.dispatch({ type: 'RESET' })}
-          style={{
-            padding: 10,
-            backgroundColor: 'yellow',
-            borderRadius: 5,
-          }}>
-          <Text>Reset</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+const Child = () => {
+  return (
+    <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+      <TouchableOpacity
+        style={{padding: 10, backgroundColor: 'green', borderRadius: 5}}>
+        <Text>InCrease</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{padding: 10, backgroundColor: 'red', borderRadius: 5}}>
+        <Text>DeCrease</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          padding: 10,
+          backgroundColor: 'yellow',
+          borderRadius: 5,
+        }}>
+        <Text>Reset</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default connect()(Child);
+export default Child;
