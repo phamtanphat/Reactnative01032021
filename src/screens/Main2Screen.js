f7680c4c8dcb4339d569f17b2864f1122f044cf9
phamtanphat <phatdroid94@gmail.com>
2021-04-09 20:59:39 +0700

    useState

--- src/screens/Main2Screen.js
+++ src/screens/Main2Screen.js
@@ -1,23 +1,27 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
 const Main2Screen = () => {
+  const [count, setCount] = useState(0);
   return (
     <View style={{flex: 1, justifyContent: 'center'}}>
       <Text style={{alignSelf: 'center', fontSize: 30, marginBottom: 10}}>
-        Count : 0
+        Count : {count}
       </Text>
       <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
         <TouchableOpacity
+          onPress={() => setCount(count + 1)}
           style={{padding: 10, backgroundColor: 'green', borderRadius: 5}}>
           <Text>InCrease</Text>
         </TouchableOpacity>
         <TouchableOpacity
+          onPress={() => setCount(count - 1)}
           style={{padding: 10, backgroundColor: 'red', borderRadius: 5}}>
           <Text>DeCrease</Text>
         </TouchableOpacity>
         <TouchableOpacity
+          onPress={() => setCount(0)}
           style={{
             padding: 10,
             backgroundColor: 'yellow',
